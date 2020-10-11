660792a0a7962c2f25314b55c66e9aa1ee685118
Yatish Kelkar <yatish1606@gmail.com>
2020-10-11 19:36:00 +0530

    Added content

--- src/components/AboutMe.jsx
+++ src/components/AboutMe.jsx
@@ -1,4 +1,5 @@
 import React from 'react'
+import feather from 'feather-icons'
 
 import '../css/about-me.css'
 
@@ -49,48 +50,33 @@ class AboutMe extends React.Component {
 
     _onScroll = () => {
          const { length, length2, length3 } = this.state
-        // let elements = document.elementsFromPoint(0,window.innerHeight * 0.5)
-        // let SVGElements = Array.from(elements[0].children).filter(element => element.nodeName === 'svg')
-       
-        // if (!SVGElements.length || window.scrollY < window.innerHeight * 0.5) return 
+        
+        var scrollpercent = (document.documentElement.scrollTop - window.innerHeight/2) / document.querySelector('.svg1').clientHeight
+        var draw = length * scrollpercent 
+        this.setState({strokeDashoffset: length - draw, scrollPercent : scrollpercent})
 
-        // if(numberOfSVG !== SVGElements.length && (window.scrollY + 300) >= (numberOfSVG+1)*600) {
-        //     this.setState({numberOfSVG: this.state.numberOfSVG + 1})
-        // }
-    
-        // if(numberOfSVG) {
-            var scrollpercent = (document.documentElement.scrollTop - window.innerHeight/2) / document.querySelector('.svg1').clientHeight
-            var draw = length * scrollpercent 
-            this.setState({strokeDashoffset: length - draw, scrollPercent : scrollpercent})
+        var scrollpercent2 = (document.documentElement.scrollTop - window.innerHeight + 650) / document.querySelector('.svg2').clientHeight
+        var draw2 = length2 * scrollpercent2 
+        this.setState({strokeDashoffset2: length2 - draw2, scrollPercent2 : scrollpercent2})
 
-            var scrollpercent2 = (document.documentElement.scrollTop - window.innerHeight + 650) / document.querySelector('.svg2').clientHeight
-            var draw2 = length2 * scrollpercent2 
-            this.setState({strokeDashoffset2: length2 - draw2, scrollPercent2 : scrollpercent2})
+        var scrollpercent3 = (document.documentElement.scrollTop - window.innerHeight + 850) / document.querySelector('.svg3').clientHeight
+        var draw3 = length3 * scrollpercent3 
+        this.setState({strokeDashoffset3: length3 - draw3, scrollPercent3 : scrollpercent3})
 
-            var scrollpercent3 = (document.documentElement.scrollTop - window.innerHeight + 1950) / document.querySelector('.svg3').clientHeight * 0.5
-            var draw3 = length3 * scrollpercent3 
-            this.setState({strokeDashoffset3: length3 - draw3, scrollPercent3 : scrollpercent3})
-
-            //console.log(SVGElements[numberOfSVG-1], scrollpercent)
-
-            //this.setState(prevState => ({strokeDashoffset: {...prevState.strokeDashoffset, [prevState.strokeDashoffset[numberOfSVG]] : length - draw}}))
-            //this.setState(prevState => ({strokeDasharray: {...prevState.strokeDasharray, [prevState.strokeDashoffset[0]] : scrollpercent}}))
-            
-            // let fakeStrokeDashArrays = [...this.state.strokeDasharray]
-            // fakeStrokeDashArrays[numberOfSVG-1] = scrollpercent
-            // this.setState({strokeDasharray: fakeStrokeDashArrays})
-
-            // let fakeStrokeDashOffset = [...this.state.strokeDashoffset]
-            // fakeStrokeDashOffset[numberOfSVG-1] = length - draw
-            // this.setState({strokeDashoffset: fakeStrokeDashOffset})
-            //this.setState(({strokeDasharray}) => ({strokeDasharray : [...strokeDasharray.slice(0,1), {...strokeDasharray[1], 0 : scrollpercent}, strokeDasharray.slice(2)]}))
-        // }
     }
 
+    stringToHTML = str => {
+        var dom = document.createElement('div');
+        dom.innerHTML = str;
+        return dom;
+    }
 
     render() {
-        //console.log( this.state.strokeDashoffset)
-        console.log(window.scrollY)
+        
+        console.log( this.stringToHTML(feather.icons.x.toSvg()))
+        let r = this.stringToHTML(feather.icons.x.toSvg())
+        const leftSpace = document.documentElement.clientWidth * 0.235  + 14
+
         return (
             <div style={{backgroundColor:'black', color:'white', marginTop:60,marginBottom:1000}} className="parentDiv" ref={this.parentReference}>
                 <div className="svgDiv" style={{display:'flex', flexDirection:'column',alignItems:'center', width:'100vw'}}>
@@ -114,10 +100,7 @@ class AboutMe extends React.Component {
                         <stop offset="1" stop-color="#44d0ff" />
                         </linearGradient>
                     </svg>
-                    {/* <p style={{position:'absolute', marginTop:170, left:'30%', fontFamily:'Consolas', fontSize:14, visibility: window.scrollY > window.innerHeight * 0.5 ? 'visible' : 'hidden', transition: 'visibility 0.4s, opacity 0.4s linear', opacity: window.scrollY > window.innerHeight* 0.6 ? 1 : 0, color:'#ababab' }}>document.querySelector('div').innerHTML = Lorem ipsum</p>
-                    <p style={{position:'absolute', marginTop:250, left:'25%', fontFamily:'Consolas', fontSize:14, visibility: window.scrollY > window.innerHeight * 0.5 ? 'visible' : 'hidden', transition: 'visibility 0.4s, opacity 0.4s linear', opacity: window.scrollY > window.innerHeight* 0.6 ? 1 : 0, color:'#ababab' }}>document.querySelector('div').innerHTML = Lorem ipsum</p> */}
-                    {/* <img src="https://im7.ezgif.com/tmp/ezgif-7-9f0be61aa3b9.gif" style={{position:'absolute', left:'30%', marginTop:160, zIndex:9, borderRadius:100, background:'linear-gradient(to bottom, #000000 70%, #00000000 )'}}/> */}
-
+                    
                     {/* Second SVG */}
                     <h2 className="about-me-text" style={{right:'15%', marginBottom:0, zIndex:10, top: window.innerHeight * 1.1 + 470, visibility: this.state.scrollPercent > 1 ? 'visible' : 'hidden', transition: 'visibility 0.4s, opacity 0.4s linear', opacity: this.state.scrollPercent > 1 ? 1 : 0 }}>and develop software</h2>
                     <svg className="svg2" height={360} width="70vw" style={{alignSelf:'center', display:'absolute', flexDirection:'column', alignItems:'center', margin: '0 auto 0 auto', padding:0, paddingLeft:'20%', transform: 'scale(-1,1)', top: -30,}} >
@@ -137,29 +120,6 @@ class AboutMe extends React.Component {
                         <stop offset="1" stop-color="#F8CC05" />
                         </linearGradient>
                     </svg>
-
-                    {/* Third SVG */}
-                    {/* <h2 className="about-me-text" style={{left:'15%',top: window.innerHeight * 1.1 + 900, visibility: this.state.scrollPercent > 0 ? 'visible' : 'hidden', transition: 'visibility 0.3s, opacity 0.2s linear', opacity: this.state.scrollPercent > 2 ? 1 : 0}}>I focus on User Experience</h2>
-                    <svg className="svg1" height={360} width="70vw" style={{alignSelf:'center', display:'flex', flexDirection:'column', alignItems:'center', margin: 'auto', paddingLeft:'15%',marginTop:70, }} >
-                        <path 
-                            className="svgPath"
-                            d = "M 50 50 Q 50 200 150 200 Q 150 200 200 200 Q 300 200 350 200 C 400 200 500 200 550 200 C 550 200 650 200 650 200 C 750 200 750 300 750 350 "  
-                            fill="none" 
-                            stroke="green" 
-                            strokeWidth={20}
-                            strokeLinecap="round"
-                            strokeDashoffset={this.state.strokeDashoffset} 
-                            strokeDasharray={this.state.strokeDasharray}
-                            strokeLinejoin="round"
-                            style={{stroke: 'url(#linear-gradient3)', width:'100%', margin:'auto'}}
-                        />
-                        <linearGradient id="linear-gradient3" x1="6.49" y1="151.03" x2="244.48" y2="151.03"
-                        gradientUnits="userSpaceOnUse">
-                        <stop offset="0" stop-color="#04CE1A" />
-                        <stop offset="1" stop-color="#D0F205" />
-                        </linearGradient>
-                    </svg> */}
-
                 </div>
                 
                 <div className='swipedown' style={{display: window.scrollY > window.innerHeight * 1.1 + 300 ? '' : 'none',width: '15vw', height:'15vw', borderRadius:200, marginLeft:'19%', backgroundColor:'#F8CC05', top: -10, position:'relative', zIndex:10}}>
@@ -168,32 +128,51 @@ class AboutMe extends React.Component {
                     </div>
                 </div>
                 
-                <svg className="svg3" height={600} width="40vw" style={{display: window.scrollY > window.innerHeight * 1.1 + 600 ? '' : 'none',alignSelf:'center', flexDirection:'column', alignItems:'center', margin: '0 auto 0 auto', padding:0, top: -60,  paddingLeft:'23%', position:'relative'}} >
+                <svg className="svg3" height={700} width="40vw" style={{display: window.scrollY > window.innerHeight * 1.1 + 600 ? '' : 'none',alignSelf:'center', flexDirection:'column', alignItems:'center', margin: '0 auto 0 auto', padding:0, top: -60,  paddingLeft:'23%', position:'relative'}} >
                         <path 
                             className="svgPath3"
-                            d = "M 50 50 C 50 300 50 300 50 550 "  
+                            d = "M 50 50 C 50 300 50 300 50 650 "  
                             fill="none" 
                             strokeWidth={20}
                             strokeLinecap="round"
                             strokeDashoffset={this.state.strokeDashoffset3} 
                             strokeDasharray={this.state.strokeDasharray3}
-                            stroke="#F8CC05"
-                            style={{width:'100%', margin:'0 auto',}}
+                            // stroke="#F8CC05"
+                            style={{width:'100%', margin:'0 auto',stroke: 'url(#linear-gradient3)'}}
                         />
-                        <linearGradient id="linear-gradient3" x1="3.49" y1="51.03" x2="44.48" y2="51.03"
+                        <linearGradient id="linear-gradient3" x1="0%" y1="10%" x2="0%" y2="100%"
                         gradientUnits="userSpaceOnUse">
-                        <stop offset="0" stop-color="red" />
-                        <stop offset="1" stop-color="green" />
+                        <stop offset="0" stop-color="#F8CC05" />
+                        <stop offset="0.25" stop-color="#0AA71D" />
+                        <stop offset="0.5" stop-color="#0AA777" />
+                        <stop offset="1" stop-color="#0A9EA3" />
+                        <stop offset="1" stop-color="#0996CE" />
                         </linearGradient>
+
                 </svg>
 
-                <div className="who-am-i" style={{width: '50vw', height:'30vw', marginLeft:'37%', backgroundColor:'transparent', top: -0, position:'absolute', top: window.innerHeight * 1.1 + 800, visibility: window.scrollY > window.innerHeight * 1.1 + 300 ? 'visible' : 'hidden',opacity: window.scrollY > window.innerHeight * 1.1 + 300 ? 1 : 0, transition: 'visibility 0.4s , opacity 0.4s linear'}}>
+                
+                <div className="skill" style={{left: leftSpace, marginTop: 180,backgroundColor:'#0AA71D',display: window.scrollY > window.innerHeight * 1.1 + 690 ? '' : 'none',}}>{}</div>
+                <div className="skill" style={{left: leftSpace, marginTop: 310,backgroundColor:'#0AA777',display: window.scrollY > window.innerHeight * 1.1 + 840 ? '' : 'none',}}>
+
+                </div>
+                <div className="skill" style={{left: leftSpace, marginTop: 440,backgroundColor:'#0A9EA3',display: window.scrollY > window.innerHeight * 1.1 + 1000 ? '' : 'none',}}>
+
+                </div>
+                <div className="skill" style={{left: leftSpace, marginTop: 570,backgroundColor:'#0996CE',display: window.scrollY > window.innerHeight * 1.1 + 1160 ? '' : 'none',}}>
+
+                </div>
+
+                <div className="who-am-i" style={{width: '50vw', height:'30vw', marginLeft:'37%', backgroundColor:'transparent', position:'absolute', top: window.innerHeight * 1.1 + 850, visibility: window.scrollY > window.innerHeight * 1.1 + 300 ? 'visible' : 'hidden',opacity: window.scrollY > window.innerHeight * 1.1 + 300 ? 1 : 0, transition: 'visibility 0.4s , opacity 0.4s linear'}}>
                     <h2>But who am I ?</h2>
                     <p>I am Yatish Kelkar. I am currently interning at FortyTwo Labs as a Product Designer</p>
                     <p>I study at PVG's COET Pune and am 20 years old</p>
-                    <p>What I work with:</p>
-                
-                
+                    <p>I love to code, develop software and cook chicken. And, ofcourse, Netflix</p>
+                </div>
+
+                <div className="who-am-i" style={{width: '50vw', height:'30vw', marginLeft:'37%', backgroundColor:'transparent', position:'absolute', top: window.innerHeight * 1.1 + 1150, visibility: window.scrollY > window.innerHeight * 1.1 + 600 ? 'visible' : 'hidden',opacity: window.scrollY > window.innerHeight * 1.1 + 600 ? 1 : 0, transition: 'visibility 0.4s , opacity 0.4s linear'}}>
+                    <h4>Snippets</h4>
+                    
                 </div>
 
 
